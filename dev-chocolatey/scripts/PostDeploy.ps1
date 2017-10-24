f2d078bd5efe85ba0aa467a9305b84a6ce579921
snagrom <5047858+snagrom@users.noreply.github.com>
2017-10-24 11:31:05 -0500

    Update PostDeploy.ps1

--- dev-chocolatey/scripts/PostDeploy.ps1
+++ dev-chocolatey/scripts/PostDeploy.ps1
@@ -1,3 +1,4 @@
-Set-Location $env:userprofile\desktop
+New-Item C:\PartsUnlimitedHOL -type directory -force
+Set-Location C:\PartsUnlimitedHOL
 git clone https://github.com/Microsoft/PartsUnlimited.git
 Remove-Item .\PartsUnlimited\src\PartsUnlimitedWebsite\node_modules -Force -Recurse
