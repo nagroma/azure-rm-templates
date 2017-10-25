41d0b522c3f64d6afee2f2d86d039fed53585fba
snagrom <5047858+snagrom@users.noreply.github.com>
2017-10-24 19:27:31 -0500

    Update PostDeploy.ps1

--- dev-chocolatey/scripts/PostDeploy.ps1
+++ dev-chocolatey/scripts/PostDeploy.ps1
@@ -1,30 +1,16 @@
 param([Parameter(Mandatory=$true)][string]$chocoPackages)
+param([Parameter(Mandatory=$true)][string]$vmAdminUserName)
+param([Parameter(Mandatory=$true)][SecureString]$vmAdminPassword)
+
 cls
 
 New-Item "c:\choco" -type Directory -force | Out-Null
 $LogFile = "c:\choco\Script.log"
 $chocoPackages | Out-File $LogFile -Append
+$vmAdminUserName | Out-File $LogFile -Append
+$vmAdminPassword | Out-File $LogFile -Append
 
-# Get username/password & machine name
-$userName = "artifactInstaller"
-[Reflection.Assembly]::LoadWithPartialName("System.Web") | Out-Null
-$password = $([System.Web.Security.Membership]::GeneratePassword(12,4))
-$cn = [ADSI]"WinNT://$env:ComputerName"
-
-# Create new user
-$user = $cn.Create("User", $userName)
-$user.SetPassword($password)
-$user.SetInfo()
-$user.description = "Choco artifact installer"
-$user.SetInfo()
-
-# Add user to the Administrators group
-$group = [ADSI]"WinNT://$env:ComputerName/Administrators,group"
-$group.add("WinNT://$env:ComputerName/$userName")
-
-# Create pwd and new $creds for remoting
-$secPassword = ConvertTo-SecureString $password -AsPlainText -Force
-$credential = New-Object System.Management.Automation.PSCredential("$env:COMPUTERNAME\$($username)", $secPassword)
+$credential = New-Object System.Management.Automation.PSCredential("$env:COMPUTERNAME\$($vmAdminUserName)", $vmAdminPassword)
 
 # Ensure that current process can run scripts. 
 #"Enabling remoting" | Out-File $LogFile -Append
@@ -53,9 +39,3 @@ Invoke-Command -ScriptBlock {git clone https://github.com/Microsoft/PartsUnlimit
 Invoke-Command -ScriptBlock {Remove-Item .\PartsUnlimited\src\PartsUnlimitedWebsite\node_modules -Force -Recurse} -ComputerName $env:COMPUTERNAME -Credential $credential | Out-File $LogFile -Append
 
 Disable-PSRemoting -Force
-
-# Delete the artifactInstaller user
-$cn.Delete("User", $userName)
-
-# Delete the artifactInstaller user profile
-gwmi win32_userprofile | where { $_.LocalPath -like "*$userName*" } | foreach { $_.Delete() }
