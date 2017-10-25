8e228eddd9b8431d0d410f37aa7fa99110f194b1
snagrom <5047858+snagrom@users.noreply.github.com>
2017-10-24 21:50:52 -0500

    Update PostDeploy.ps1

--- dev-chocolatey/scripts/PostDeploy.ps1
+++ dev-chocolatey/scripts/PostDeploy.ps1
@@ -32,13 +32,23 @@ $chocoPackages.Split(";") | ForEach {
     Invoke-Command -ScriptBlock $sb -ArgumentList $chocoPackages -ComputerName $env:COMPUTERNAME -Credential $credential | Out-Null
 }
 
+$AddedLocation ="$env:userprofile\AppData\Roaming\npm"
+$Reg = "Registry::HKLM\System\CurrentControlSet\Control\Session Manager\Environment"
+$OldPath = (Get-ItemProperty -Path "$Reg" -Name PATH).Path
+"OldPath: $OldPath" | Out-File $LogFile -Append
+$NewPath= $OldPath + ’;’ + $AddedLocation
+Set-ItemProperty -Path "$Reg" -Name PATH –Value $NewPath
+$UpdatedPath = (Get-ItemProperty -Path "$Reg" -Name PATH).Path
+"UpdatedPath: $UpdatedPath" | Out-File $LogFile -Append
+
 Invoke-Command -ScriptBlock {refreshenv} -ComputerName $env:COMPUTERNAME -Credential $credential | Out-File $LogFile -Append
-Invoke-Command -ScriptBlock {npm install -g bower} -ComputerName $env:COMPUTERNAME -Credential $credential | Out-File $LogFile -Append
+Invoke-Command -ScriptBlock {npm install bower -g} -ComputerName $env:COMPUTERNAME -Credential $credential | Out-File $LogFile -Append
+Invoke-Command -ScriptBlock {npm install grunt-cli -g} -ComputerName $env:COMPUTERNAME -Credential $credential | Out-File $LogFile -Append
 Invoke-Command -ScriptBlock {npm install -g grunt-cli} -ComputerName $env:COMPUTERNAME -Credential $credential | Out-File $LogFile -Append
 Invoke-Command -ScriptBlock {Copy-Item C:\Python27\python.exe C:\Python27\python2.exe} -ComputerName $env:COMPUTERNAME -Credential $credential | Out-File $LogFile -Append
 Invoke-Command -ScriptBlock {New-Item c:\PartsUnlimitedHOL -type directory -force} -ComputerName $env:COMPUTERNAME -Credential $credential | Out-File $LogFile -Append
 Invoke-Command -ScriptBlock {CD C:\PartsUnlimitedHOL\} -ComputerName $env:COMPUTERNAME -Credential $credential | Out-File $LogFile -Append
 Invoke-Command -ScriptBlock {git clone https://github.com/Microsoft/PartsUnlimited.git C:\PartsUnlimitedHOL\} -ComputerName $env:COMPUTERNAME -Credential $credential | Out-File $LogFile -Append
-Invoke-Command -ScriptBlock {Remove-Item .\PartsUnlimited\src\PartsUnlimitedWebsite\node_modules -Force -Recurse} -ComputerName $env:COMPUTERNAME -Credential $credential | Out-File $LogFile -Append
+#Invoke-Command -ScriptBlock {Remove-Item .\PartsUnlimited\src\PartsUnlimitedWebsite\node_modules -Force -Recurse} -ComputerName $env:COMPUTERNAME -Credential $credential | Out-File $LogFile -Append
 
 Disable-PSRemoting -Force
